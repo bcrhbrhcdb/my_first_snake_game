88e81146039a7b7ef621f139bfbadc1ca39dd32e
bcrhbrhcdb <apet2804@mpsedu.org>
2024-01-12 12:19:55 -0600

    s

--- script.js
+++ script.js
@@ -55,4 +55,15 @@ function startGame() {
     }
   }
 
+  function moveSnake(squares) {
+    let tail = currentSnake.pop();
+    squares[tail].classList.remove("snake");
+    currentSnake.unshift(currentSnake[0] + direction);
+    // movement ends here
+    eatApple(squares, tail);
+    squares[currentSnake[0]].classList.add("snake");
+  }
+
+  
+
   
